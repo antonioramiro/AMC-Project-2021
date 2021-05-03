b65d96f01f2a99f40e37321d6ab6b7a7b46d5dc0
Maria Brazão <maria.brazao@tecnico.ulisboa.pt>
2021-05-03 22:09:29 +0100

    Update Graph.java

--- src/CancerClassifier/Graph.java
+++ src/CancerClassifier/Graph.java
@@ -1,6 +1,27 @@
 package CancerClassifier;
 
+import java.util.Arrays;
+
 public class Graph {
+	int dim;
+	int [][] ma;
+	
+	public Graph(int d) {
+		this.dim = d;
+		this.ma = new int [d][d];
+	}
+	
+	public int getDim() {
+		return this.dim;
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Graph [dim=" + dim + ", ma=" + Arrays.toString(ma) + "]";
+	}
+	
+	
 
     
 }
