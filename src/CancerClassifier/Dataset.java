15cb5211b0e772758dadff18d109186b566cf08f
António <antonio.ramiro@tecnico.ulisboa.pt>
2021-05-04 02:35:15 +0100

    Implementing Dataset (NOT TESTED)
    Needs to be tested

--- src/CancerClassifier/Dataset.java
+++ src/CancerClassifier/Dataset.java
@@ -1,26 +1,73 @@
 package CancerClassifier;
 
+//To allow ArrayList use
 import java.util.ArrayList;
 
+//Dataset stores an ArrayList of DataPoints, and its length.
 public class Dataset {
-    DataPoint 
 
+    //Class Attributes
+    int len;
+    ArrayList<DataPoint> dataList;
+    
+    //Constructor of empty Dataset
+    public Dataset(){
+        this.len = 0;
+        this.dataList = new ArrayList<DataPoint>();
+    }
 
+    //Constructor of filled Dataset
+    public Dataset(int l,  ArrayList<DataPoint> dL){
+        this.len = l;
+        this.dataList = dL;
+    }
+    
+    //Counting datapoints in the dataset, in which there is xi in index i and xi in index j, simultaneously
+    public int Count(int i, int j, double xi, double xj){
+        int counter = 0;
+        for (DataPoint dp: this.dataList){
+            if (dp.isThere(i, j, xi, xj)) counter++;
+        }
+        return counter;
+    }
+
+    //Adding a single DataPoint to the dataset
+    public void Add(DataPoint v){
+        this.len++;
+        this.dataList.add(v);
+    }
+
+    //Returning a sub-Dataset, depending on which classification is requested
+    public Dataset Fiber(String classification){
+        Dataset fiber = new Dataset();
+        for (DataPoint dp: this.dataList){
+            if (dp.getClassification() == classification){
+                fiber.Add(dp);
+            }
+        }
+        return fiber;
+
+    }
 
     //
-    public static int Count(){
-        int i = 0;
-        return i;
+    @Override
+    public String toString() {
+        return "{" +
+            " len='" + getLen() + "'" +
+            ", dataList='" + getDataList() + "'" +
+            "}";
     }
 
-    public static void Add(){
 
+    public int getLen() {
+        return this.len;
     }
 
-    //nao e void, é outra cosia
-    public static void Fiber(){
-
+    public ArrayList<DataPoint> getDataList() {
+        return this.dataList;
     }
+
+
 
     public static void main(String[] args) {
         
