879e33b2125b1794f287c9303f95b2d5515c1aa9
António <antonio.ramiro@tecnico.ulisboa.pt>
2021-05-06 20:47:08 +0100

    Solving badly-dealt-w/ conflict

--- src/CancerClassifier/Classifier.java
+++ src/CancerClassifier/Classifier.java
@@ -1,34 +1,50 @@
 package CancerClassifier;
 
+import java.util.Array;
 import java.util.ArrayList;
+
 
 public class Classifier {
 
     Dataset T;
-    Tree A;
+    Graph A;
     MRFT markov;
+    double [] Frequency;
+    
 
     ArrayList<Double[][]>[][] benignMRFT, malignantMRFT; //erro aqui
     double benignFrequency, malignantFrequency;
     
-    public Classifier( Array<ArrayList<Double[][]>>  MRFT, double[] Frequency) {
-        this.benignMRFT = MRFT[0];
-        this.malignantMRFT = MRFT[1];
-        this.benignFrequency = Frequency[0];
-        this.malignantFrequency = Frequency[1];
-    }
+	public Classifier(ArrayList<ArrayList<Double[][]>>  MRFT, double[] Frequency) {
+		this.benignMRFT = MRFT[0];
+		this.malignantMRFT = MRFT[1];
+		this.benignFrequency = Frequency[0];
+	    this.malignantFrequency = Frequency[1];
+	    }
+	
+	public int Frequency(Dataset T, int i, int xi) {
+		int freq;
+		freq = T.Count(i, xi)/T.Dim();
+		return freq;
+		
+	}
     
 
     public int Classify(int[] Xn){
-        
-        int classDim = T.dim(class);
+    	
         int p = 0;
         int diagnosis;
 
-        for(int i = 0; i < classNumber, i++){
-            if(p < MRFT.getProbability(T.Fiber(i), A, Xn)){
+        for(int i = 0; i < A.getDim(); i++){  //
+            if(p < MRFT.Prob(T.Fiber(i), A, Xn)) {
+            	
                 diagnosis = i;
+                p = MRFT.Prob(T.Fiber(i), A, Xn) * Frequency[i];
+                
             }
+ 
+        return diagnosis;
+            	
             
         }
 
