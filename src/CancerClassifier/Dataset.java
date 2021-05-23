e4015a96a62d860c7db3c703d428519aeb922dd9
António <antonio.ramiro@tecnico.ulisboa.pt>
2021-05-23 13:08:09 +0100

    Merging Datapoint w/Dataset + Documenting my part

--- src/CancerClassifier/Dataset.java
+++ src/CancerClassifier/Dataset.java
@@ -1,15 +1,58 @@
 package CancerClassifier;
 
-import java.io.Serializable;
+
 //To allow ArrayList use
 import java.util.ArrayList;
 import java.util.Arrays;
 
+class DataPoint {
+	
+    //Class Attributes
+    int[] measurements;
+    int classification;
+
+    //Constructor of DataPoint with specific measurements and classification. Array length and classification must be as expected.
+    public DataPoint(int[] m, int c){
+        this.measurements = m;
+        this.classification = c;
+    }
+
+    //getter for measurements
+    public int[] getMeasurements() {
+        return this.measurements;
+    }
+
+    //getter for tumour classification
+    public int getClassification() {
+        return this.classification;
+    }
+
+    //Searching the datapoint for xi in index i and xi in index j, simultaneously
+    public boolean isThere(int i, int j, int xi, int xj){
+            return (this.measurements[i] == xi && this.measurements[j] == xj);
+    }
+
+    //Searching the datapoint for xi in index i 
+    public boolean isThere(int i, int xi){
+        return this.measurements[i] == xi;    
+    }
+
+    //Searching the datapoint for xi in index i 
+    public boolean isClass(int i){
+        return this.classification == i;    
+    }
+
+    @Override
+    public String toString() {
+        return "(Measurements: " + Arrays.toString(getMeasurements()) + "; " +
+               "Classification: " + getClassification() + ") \n";
+    }
+
+}
+
 //Dataset stores an ArrayList of DataPoints, and its length.
-public class Dataset implements Serializable{
-	private static final long serialVersionUID = 8L;
-
-
+public class Dataset {
+	
     //Class Attributes
     ArrayList<DataPoint> dataList;
     public int[] measurementsDomain; //domain of the dataset measurements
@@ -114,9 +157,9 @@ public class Dataset implements Serializable{
     }
 
     public static void main(String[] args) { 
-       /*
+       
         //Empty Dataset
-        Dataset ds1 = new Dataset();
+        Dataset ds1 = new Dataset(10);
         System.out.println("Empty Dataset: " + ds1);
 
         //Creating Datapoints
@@ -145,13 +188,10 @@ public class Dataset implements Serializable{
         System.out.println("Dataset with 4 datapoints: " + ds1 + "\n");
 
         //Getting the size of the dataset
-        System.out.println(ds1.len());
-
-        //Dim of variable indexed in 9
-        System.out.println("Dataset with 52 datapoints: " + ds1.measurementDim(9) + "\n");
+        System.out.println("Dataset lenght: " + ds1.len());
 
         //Dim of the class
-        System.out.println("Dataset with 52 datapoints: " + ds1.classDim() + "\n");
+        System.out.println("Dataset nr of measurements " + ds1.classDim() + "\n");
 
         //Getting the fiber of 0-class tumours
         System.out.println("0-class Fiber: \n" + ds1.Fiber(0) + "\n"); 
@@ -161,32 +201,9 @@ public class Dataset implements Serializable{
 
         //Counting the number of combinations in the dataset
         System.out.println("Number of times 1 shows in variable indexed 0, and 10 in 9, respectively and simultaneosly: "
-                           + ds1.Count(0, 9, 1, 10) + "\n"); */
-                           
-        Dataset ds1 = new Dataset(4);
-
-        int[] m6 = {1,0,3,4};
-        int c6 = 0;
-        DataPoint dp6 = new DataPoint(m6,c6);
-        int[] m7 = {3,1,2,4};
-        int c7 = 0;
-        DataPoint dp7 = new DataPoint(m7,c7);
-        int[] m8 = {1,0,2,4};
-        int c8 = 1;
-        DataPoint dp8 = new DataPoint(m8,c8);
-
-        ds1.Add(dp6);
-        for(int i = 0; i < 3; i++){
-            ds1.Add(dp7);
-        }
-        ds1.Add(dp8);
-
-        System.out.println("Dataset: " + ds1);
-
+                           + ds1.Count(0, 9, 1, 10)); 
         
-        System.out.println("Nr de Class(0) " + ds1.countClassification(1));
-        System.out.println("Len total " + ds1.len());
+        //Calculating frequency of a class
         System.out.println("Frequency of class " + ds1.classFrequency(1));
-
     }    
 }
--- src/CancerClassifier/FileHandling.java
+++ src/CancerClassifier/FileHandling.java
@@ -13,139 +13,165 @@ import java.io.ObjectOutputStream;
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
 
-
+//As the name suggests, handles files
 public class FileHandling {
 
   //getDataset receives the path and imports the .csv onto a variable of the type Dataset
   public static Dataset getDataset(String path) {
 
     try {
+      //initializing the instance of the file to be read and the csv reader of said file
       File csvFile = new File(path);
       Scanner csvReader = new Scanner(csvFile);
 
-      //procedimento para a primeira linha, que definirá o nr de medições do dataset
+      //for the first line, reading the line
       String firstData = csvReader.nextLine();
-      String[] firstValues = (firstData.split(","));
-      int measurementNumber = firstValues.length - 1;
+      String[] firstValues = (firstData.split(",")); //parsing the string, splitting the commas
+      int measurementNumber = firstValues.length - 1; //getting how many measurements the line has (the last one, -1 is the class)
 
+      //now that we know the measurement number, we can create an int array, and convert the previous String array to int.array
       int[] firstMeasurements = new int[measurementNumber];
       for (int i = 0; i < measurementNumber; i++) {
         int thisMeasurement = Integer.parseInt(firstValues[i]);
         firstMeasurements[i] = thisMeasurement;
       }
 
+      //setting the class of the line (last item) to a variable
       int firstClassification = Integer.parseInt(firstValues[measurementNumber]);
 
-      Dataset newDataset = new Dataset(measurementNumber);
+      //creating a datapoint w/ the measurements and the classification
       DataPoint first = new DataPoint(firstMeasurements, firstClassification);
+
+      //creating a dataset, w/ the now know number of measurements
+      Dataset newDataset = new Dataset(measurementNumber);
+      
+      //adding the first datapoint to the dataset
       newDataset.Add(first);
 
-      // procedimento para as restantes linhas
+      //having the measurement number, repeat previous procedure to remaining lines
       while (csvReader.hasNextLine()) {
+        //initialize data separated by commas array
         String data = csvReader.nextLine();
         String[] values = (data.split(","));
 
-      int classification = Integer.parseInt(values[measurementNumber]);
-      int[] measurements = new int[measurementNumber];
+        //Setting classification
+        int classification = Integer.parseInt(values[measurementNumber]);
+        
+        //Setting measurements
+        int[] measurements = new int[measurementNumber];
+        for (int i = 0; i < firstValues.length - 1; i++) {
+          int thisMeasurement = Integer.parseInt(values[i]);
+          measurements[i] = thisMeasurement;
+        }    
 
-      for (int i = 0; i < firstValues.length - 1; i++) {
-        int thisMeasurement = Integer.parseInt(values[i]);
-        measurements[i] = thisMeasurement;
-      }    
-
+        //creating and adding datapooint do dataset
         DataPoint dp = new DataPoint(measurements, classification);
         newDataset.Add(dp);
       }
+
+      //closing the file 
       csvReader.close();
       
+      //returning the Dataset
       return newDataset;
 
-      
+      //catching exception (usually found when the file is not in the correct path)
     } catch (FileNotFoundException e) {
       e.printStackTrace();
     } 
+
+    //if try fails, return null (since try failed, the previous catch instance was executed)
     return null;
   }
 
-
+  //exporting classifier to a file, receives the dataset and the name to be given to it
   public static void exportClassifier(Dataset data, String datasetName){
+    //declaring the bundle (running chow liu, and mrft)
     ClassifierPackager  object = new ClassifierPackager(data);
+
+    //setting the name of the file
     String filename = datasetName + ".classifier";
       
-    // Serialization 
-    try
-    {   
-        //Saving of object in a file
-        FileOutputStream file = new FileOutputStream("Classifiers/" + filename);
-        ObjectOutputStream out = new ObjectOutputStream(file);
-          
-        // Method for serialization of object
-        out.writeObject(object);
-          
-        out.close();
-        file.close();
-          
-        System.out.println("Object has been serialized");
+    //Serializing
+    try{   
+      //Saving of object in a file
+      FileOutputStream file = new FileOutputStream("Classifiers/" + filename);
+      ObjectOutputStream out = new ObjectOutputStream(file);
+        
+      // Method for serialization of object
+      out.writeObject(object);
+        
+      //closing methods
+      out.close();
+      file.close();
 
-    }
-      
-    catch(IOException ex)
-    {
-        System.out.println("IOException is caught");
-        ex.printStackTrace();
+      //confirming success
+      System.out.println("Classifier exported successfully.");
+    
+      //catching exception - path not found
+    }catch(IOException ex){
+      System.out.println("IOException is caught");
+      ex.printStackTrace();
     }
   }
 
+  //importing the classifier
   public static ClassifierPackager importClassifier(String path){
-    // Deserialization
-    try
-    {   
-        // Reading the object from a file
-        FileInputStream file = new FileInputStream(path);
-        ObjectInputStream in = new ObjectInputStream(file);
-          
-        // Method for deserialization of object
-        ClassifierPackager cp = (ClassifierPackager)in.readObject();
-          
-        in.close();
-        file.close();
-          
-        System.out.println("Object has been deserialized ");
-       
-        return cp;
 
-    }
+    //Deserializing
+    try{   
+      // Reading the object from a file
+      FileInputStream file = new FileInputStream(path);
+      ObjectInputStream in = new ObjectInputStream(file);
+        
+      // Method for deserialization of object
+      ClassifierPackager cp = (ClassifierPackager)in.readObject();
       
-    catch(IOException ex)
-    {
+      //closing the methods
+      in.close();
+      file.close();
+        
+      //Confirming success
+      System.out.println("Classifier imported successfully.");
+      
+      //returning the classifier package
+      return cp;
+
+    //catching exceptions
+    }catch(IOException ex){
       ex.printStackTrace();
       System.out.println("IOException is caught");
-    }
-      
-    catch(ClassNotFoundException ex)
-    {
+    }catch(ClassNotFoundException ex){
       ex.printStackTrace();
       System.out.println("ClassNotFoundException is caught");
     }
 
+    //returning a null (shall never happen, since exceptions are above)
     ClassifierPackager error = new ClassifierPackager(null);
     return error;
   }
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
     
-    //System.out.println("\n");
-    Dataset a = FileHandling.getDataset("Datasets/diabetes.csv");
-    System.out.println("Dataset example: " + Arrays.toString(a.measurementsDomain));
+    //importing a dataset from path
+    Dataset T = FileHandling.getDataset("Datasets/diabetes.csv");
+    //proving dataset correct importing (w/o spamming the console)
+    System.out.println("Dataset domain: " + Arrays.toString(T.measurementsDomain));
 
-    FileHandling.exportClassifier(a,"diabetes");
-    ClassifierPackager b = FileHandling.importClassifier("Classifiers/diabetes.classifier");
-    System.out.println(b);
+    //exporting the classifier
+    FileHandling.exportClassifier(T,"diabetes");
+  
+    //importing the classifier
+    ClassifierPackager cp = FileHandling.importClassifier("Classifiers/diabetes.classifier");
     
-    int[] d = {1,3,0,0,0,0,0,0};
-    Classifier c = new Classifier(b.getMrft(), b.getFreq());
-    int result = c.Classify(d);
-    System.out.println(result);
-    
+    //measurement vector
+    int[] measurements = {1,3,0,0,0,0,0,0};
+
+    //constructing a classifier w/ both arrays
+    Classifier c = new Classifier(cp.getMrft(), cp.getFreq());
+
+    //classify the measurements
+    int result = c.Classify(measurements);
+    System.out.println("Diagnosis: " + result);
   }
 }
