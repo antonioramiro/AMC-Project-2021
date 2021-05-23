cbdcd40b02753f0fb5cf9b8fd8b5e1d95bd6c823
alinefragas <82322877+alinefragas@users.noreply.github.com>
2021-05-23 16:13:58 +0100

    window1 ready

--- src/CancerClassifier/window1.java
+++ src/CancerClassifier/window1.java
@@ -9,19 +9,15 @@ import javax.swing.JButton;
 import javax.swing.JFileChooser;
 
 import java.awt.event.ActionListener;
-import java.util.Arrays;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
-import javax.swing.SwingConstants;
 
 public class window1 {
 
 	private JFrame frmWindow;
 	private final JFileChooser OpenFile;
 
-	/**
-	 * Launch the application.
-	 */
+	///Launch the application.	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -35,9 +31,7 @@ public class window1 {
 		});
 	}
 
-	/**
-	 * Create the application.
-	 */
+	//Create the application.
 	public window1() {
 		initialize();
 		
@@ -45,9 +39,7 @@ public class window1 {
 		OpenFile.setFileFilter(new FileNameExtensionFilter("csv documents","csv"));
 	}
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
+	//Initialize the contents of the frame.	
 	private void initialize() {
 		frmWindow = new JFrame();
 		frmWindow.getContentPane().setBackground(Color.WHITE);
@@ -56,52 +48,58 @@ public class window1 {
 		frmWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmWindow.getContentPane().setLayout(null);
 		
+		//Instructions label
 		JLabel instructionsLbl = new JLabel("To create a classifier, select a dataset to import");
 		instructionsLbl.setBounds(10, 23, 376, 14);
 		frmWindow.getContentPane().add(instructionsLbl);
 		
+		//Dataset path label
 		JLabel PathLbl = new JLabel("");
 		PathLbl.setBackground(Color.BLACK);
 		PathLbl.setBounds(133, 48, 493, 19);
 		frmWindow.getContentPane().add(PathLbl);
-			
-		JButton openBtn = new JButton("Choose file...");
-		openBtn.addActionListener(new ActionListener() {
+		
+		//Open file button 
+		JButton OpenBtn = new JButton("Choose file...");
+		OpenBtn.setBounds(10, 44, 113, 23);
+		frmWindow.getContentPane().add(OpenBtn);
+		
+		//Let's user select a dataset when Open button is activated
+		OpenBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(OpenFile.showOpenDialog(frmWindow) == JFileChooser.APPROVE_OPTION) {
-					String path = OpenFile.getSelectedFile().getAbsolutePath();
-					PathLbl.setText(path);
+				if(OpenFile.showOpenDialog(frmWindow) == JFileChooser.APPROVE_OPTION) { //opens file dialog
+					String path = OpenFile.getSelectedFile().getAbsolutePath(); //gets file path
+					PathLbl.setText(path); //sets path label with dataset path
 			}
 		}});
-		openBtn.setBounds(10, 44, 113, 23);
-		frmWindow.getContentPane().add(openBtn);
-		
+
+		//Update label
 		JLabel updateLbl = new JLabel("");
 		updateLbl.setForeground(Color.BLACK);
 		updateLbl.setBounds(10, 70, 517, 22);
 		frmWindow.getContentPane().add(updateLbl);
 		
-		JButton GoBtn = new JButton("Import");
-		GoBtn.addActionListener(new ActionListener() {
+		//Import button 
+		JButton ImportBtn = new JButton("Import");
+		ImportBtn.setBounds(537, 70, 89, 23);
+		frmWindow.getContentPane().add(ImportBtn);
+		
+		//Imports a dataset and creates a classifier when ImportButton is activated
+		ImportBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) { 
 				String path = PathLbl.getText();
-			if(path.isBlank()){
+			if(path.isBlank()){ //makes sure a path was chosen
 				updateLbl.setForeground(Color.RED);
-				updateLbl.setText("Please choose a dataset");
+				updateLbl.setText("Please choose a dataset"); 
 			}else{
-				Dataset T = FileHandling.getDataset(path);
-				String FinalPath = path.split("\\\\")[(path.split("\\\\").length)-1].split("\\.")[0];
-				FileHandling.exportClassifier(T, FinalPath);
-				updateLbl.setForeground(Color.GREEN);
+				Dataset T = FileHandling.getDataset(path); //gets selected dataset
+				String FinalPath = path.split("\\\\")[(path.split("\\\\").length)-1].split("\\.")[0]; //prepares classifier name
+				FileHandling.exportClassifier(T, FinalPath); //exports classifier with right name
+				updateLbl.setForeground(Color.GREEN); 
 				updateLbl.setText(FinalPath + "classifier has been created, you may close this window now");
-				
 				}
 			}
 		});
-		GoBtn.setBounds(537, 70, 89, 23);
-		frmWindow.getContentPane().add(GoBtn);
-		
-		
 		
 	}
 }
